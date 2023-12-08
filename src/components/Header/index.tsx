ab517190ffb0a3de437eb11ade8478fd5960d35f
Lucas Mazini <31120887+LukasMM@users.noreply.github.com>
2023-12-08 15:26:53 -0300

    Add files via upload

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+import Logo from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <img src={Logo} alt="logo da aplicação" />
+    </header>
+  )
+}
